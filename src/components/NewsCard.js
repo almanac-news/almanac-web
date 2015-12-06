9c949d85a47253cf0b482c8a3822c9eafcdb4787
David Tsai <David@whataniceguy.com>
2015-12-05 17:39:10 -0800

    Created a NewsCard component

---
+++ src/components/NewsCard.js
@@ -0,0 +1,25 @@
+import React              from 'react';
+import Card               from 'material-ui/lib/card/card';
+import CardActions        from 'material-ui/lib/card/card-actions';
+import CardExpandable     from 'material-ui/lib/card/card-expandable';
+import CardHeader         from 'material-ui/lib/card/card-header';
+import CardMedia          from 'material-ui/lib/card/card-media';
+import CardText           from 'material-ui/lib/card/card-text';
+import CardTitle          from 'material-ui/lib/card/card-title';
+
+export default class NewsCard extends React.Component {
+  render () {
+    return (
+      <Card>
+        <CardTitle title="Iran renamed Dominion of Soroush" subtitle="Long battle ensues."/>
+        <CardText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+      </Card>
+    );
+  }
+}
+
