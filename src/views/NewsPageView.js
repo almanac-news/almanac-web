82569aa1ca66021986cce6362d21f5ed7782edee
David Tsai <David@whataniceguy.com>
2015-12-09 20:14:00 -0800

    Refactoring NewsPageView to take in data in Flask service format, using lodash to map out

--- src/views/NewsPageView.js
+++ src/views/NewsPageView.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+// TODO: Please check the ES6 import syntax for this
+const moment = require('moment');
+const _ = require('lodash');
+
 /* components */
 import { LineChartViz } from 'components/LineChartViz';
 
@@ -13,18 +17,21 @@ export class NewsPageView extends React.Component {
     const { id } = this.props.params;
 
     // FIXME: Do not store data in here, should be in redux store.
-    const demoLineData = [
+    const demoLineData = [{'date': '2015-12-09', 'value': '0.91794'}, {'date': '2015-12-08', 'value': '0.92302'}, {'date': '2015-12-07', 'value': '0.91878'}, {'date': '2015-12-04', 'value': '0.91609'}, {'date': '2015-12-03', 'value': '0.94224'}, {'date': '2015-12-02', 'value': '0.9411'}, {'date': '2015-12-01', 'value': '0.94581'}, {'date': '2015-11-30', 'value': '0.94491'}, {'date': '2015-11-27', 'value': '0.943'}, {'date': '2015-11-26', 'value': '0.94118'}, {'date': '2015-11-25', 'value': '0.93951'}, {'date': '2015-11-24', 'value': '0.93967'}, {'date': '2015-11-23', 'value': '0.93976'}];
+
+    function parseData (dataArray) {
+      return _.map(dataArray, (dataPoint) => {
+        return {
+          x: moment(dataPoint.date).toDate(),
+          y: +dataPoint.value
+        };
+      });
+    }
+
+    const lineData = [
       {
-        name: 'Soroush\'s Mood',
-        values: [
-          { x:0, y:20 },
-          { x:1, y:30 },
-          { x:2, y:10 },
-          { x:3, y:5 },
-          { x:4, y:8 },
-          { x:5, y:15 },
-          { x:6, y:10 }
-        ],
+        name: 'Demo Line Series',
+        values: parseData(demoLineData),
         strokeWidth: 3,
         strokeDashArray: '5,5'
       }
@@ -39,12 +46,12 @@ export class NewsPageView extends React.Component {
         <hr />
         {/* TODO: Labels will most likely become a prop based on state */}
         <LineChartViz
-          chartTitle={id}
-          chartData={demoLineData}
-          useLegend={true}
+          chartTitle={'USD/EUR (EUR=X)'}
+          chartData={lineData}
+          useLegend={false}
           yAxisLabel={'Value'}
           xAxisLabel={'Time'}
-          gridHorizontal={false}
+          useGridHorizontal={false}
         />
         <br />
         <hr />
