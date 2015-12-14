9990f3bda59637af11a4d77056702bd7f65caefa
David Tsai <David@whataniceguy.com>
2015-12-14 15:31:20 -0800

    Adding in a NotFoundView for wildcard routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,13 @@ import CoreLayout            from 'layouts/CoreLayout';
 import HomeView              from 'views/HomeView';
 import AboutView             from 'views/AboutView';
 import NewsPageView          from 'views/NewsPageView';
+import NotFoundView from 'views/NotFoundView';
 
 export default (
   <Route path='/' component={CoreLayout}>
     <IndexRoute component={HomeView} />
     <Route path='about' component={AboutView} />
-    <Route path='news' component={NewsPageView} />
     <Route path='news/:id' component={NewsPageView} />
+    <Route path='*' component={NotFoundView} />
   </Route>
 );
